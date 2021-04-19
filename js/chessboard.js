d6f9eb0065fbe59e2e5d549ca7c39f1d19db9f38
Benji Tusk <benjitusk1@gmail.com>
2021-04-19 15:52:03 +0300

    new ChessBoard class with renderBoard updatePieces renderpieces funcs

---
+++ js/chessboard.js
@@ -0,0 +1,33 @@
+class ChessBoard {
+  constructor() {
+    this.width = 8;
+    this.height = 8;
+    this.size = 90;
+    this.pieces = []
+  }
+
+  renderBoard() {
+    for (var i = 0; i < this.width; i++) {
+      for (var j = 0; j < this.height; j++) {
+        // if i and j are BOTH odd, or BOTH even, make the square light.
+        if (( /*Both even*/ i % 2 == 0 && j % 2 == 0) || ( /*Both odd*/ i % 2 == 1 && j % 2 == 1)) {
+          fill(255);
+        } else {
+          fill(0);
+        }
+        rect(i * this.size, j * this.size, this.size);
+      }
+    }
+  }
+
+  updatePieces(pieces) {
+    this.pieces = pieces;
+  }
+
+  renderPieces() {
+    for (let piece in this.pieces) {
+      piece.render();
+    }
+  }
+
+}
