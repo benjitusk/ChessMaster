814e7809bc409c9f1a5eede380dcafb8ce6b9234
Benji Tusk <benjitusk1@gmail.com>
2021-04-19 15:52:52 +0300

    add more properties to Piece(), and a render func

--- js/piece.js
+++ js/piece.js
@@ -1,13 +1,19 @@
 class Piece {
-  constructor(type, team, defaultPos) {
+  constructor(type, team, pos) {
     this.type = type;
     this.team = team;
-    this.default = defaultPos;
+    this.live = true;
+    this.selected = false;
+    this.pos = pos;
   }
   move(loc) {
 
   }
   validateMove(loc) {
 
+  }
+
+  render() {
+    // get image based off this.type then render it at this.pos
   }
 }
