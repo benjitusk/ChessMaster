39091de58aeafa1854ed80075d2961161a97f1b4
Benji Tusk <benjitusk1@gmail.com>
2021-04-19 14:46:18 +0300

    set up skeleton piece class

---
+++ piece.js
@@ -0,0 +1,13 @@
+class Piece {
+  constructor(type, team, defaultPos) {
+    this.type = type;
+    this.team = team;
+    this.default = defaultPos;
+  }
+  move(loc) {
+
+  }
+  validateMove(loc) {
+
+  }
+}
