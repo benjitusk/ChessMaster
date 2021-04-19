e3bba35e2431e797b5273ec041ec13dfdfd2ee2d
Benji Tusk <benjitusk1@gmail.com>
2021-04-19 18:05:36 +0300

    set default positions and pass them to ChessBoard()

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 // ==== Global Variables ====
 let currentMove = "white";
 let chessBoard;
+let defaultPositions = [
+  ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook'],
+  ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', ],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
+  ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', ],
+  ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook']
+];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  chessBoard = new ChessBoard();
+  chessBoard = new ChessBoard(defaultPositions);
 }
 
 function draw() {
