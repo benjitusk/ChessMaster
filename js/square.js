8b4a71d0ab88bfcbbb123b83442f52df5cc17f5c
Benji Tusk <benjitusk1@gmail.com>
2021-04-27 12:38:52 +0300

    added triangles to square to mark if in check

--- js/square.js
+++ js/square.js
@@ -66,14 +66,32 @@ class Square {
       fill(0);
       text("(" + this.pos.x + "," + this.pos.y + ")", this.pos.x * this.size + this.size / 2, this.pos.y * this.size + this.size / 2);
     }
+    if (config.debug && this.debugMessage) {
+      text(this.debugMessage, this.pos.x * this.size + textSize(), this.pos.y * this.size + textSize());
+
+    }
     if (config.showAllSquaresInCheck) {
       if (this.whiteCheck) {
         fill(255, 0, 0, 127);
-        circle((this.pos.x * this.size) + this.size / 2, (this.pos.y * this.size) + this.size / 2, this.size / 3);
+        let x1 = this.pos.x * this.size + (this.size / 2);
+        let y1 = this.pos.y * this.size;
+        let x2 = this.pos.x * this.size + this.size;
+        let y2 = this.pos.y * this.size;
+        let x3 = this.pos.x * this.size + this.size;
+        let y3 = this.pos.y * this.size + (this.size / 2);
+        triangle(x1, y1, x2, y2, x3, y3);
+        // circle((this.pos.x * this.size) + this.size / 2, (this.pos.y * this.size) + this.size / 2, this.size / 3);
       }
       if (this.blackCheck) {
         fill(0, 0, 255, 127);
-        circle((this.pos.x * this.size) + this.size / 2, (this.pos.y * this.size) + this.size / 2, this.size / 3);
+        let x1 = this.pos.x * this.size + (this.size / 2);
+        let y1 = this.pos.y * this.size;
+        let x2 = this.pos.x * this.size + this.size;
+        let y2 = this.pos.y * this.size;
+        let x3 = this.pos.x * this.size + this.size;
+        let y3 = this.pos.y * this.size + (this.size / 2);
+        triangle(x1, y1, x2, y2, x3, y3);
+        // circle((this.pos.x * this.size) + this.size / 2, (this.pos.y * this.size) + this.size / 2, this.size / 3);
       }
     }
   }
